b18841b2fd82cadec26c9518225a7b4a7e28aa13
RafaRed <rafaelszcardoso@gmail.com>
2022-03-20 00:01:16 -0300

    Importing pull requests through csv files, pull requests layour, collapse pr, Markdown basic support

--- web3/src/pages/SetupContest.js
+++ web3/src/pages/SetupContest.js
@@ -1,10 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import "../css/SetupContest.css";
 import injectSheet from "react-jss";
+import Papa from "papaparse";
+import ReactMarkdown from 'react-markdown'
 
 function SetupContest() {
 	const [username, setUsername] = useState("");
+	const [array, setArray] = useState();
+	const fileInput = useRef(null);
+	const selectFile = () => {
+		fileInput.current.click();
+	};
+
 	return (
 		<>
 			<Navbar menu="explore" username={username} setUsername={setUsername} />
@@ -44,17 +52,87 @@ function SetupContest() {
 						<div className="pull-header">
 							<h2 className="block-title">Pull Requests</h2>
 							<div className="buttons">
-								<button className="import">IMPORT CSV</button>
+								<input
+									ref={fileInput}
+									onChange={(e) => upload(e, setArray)}
+									type="file"
+									style={{ display: "none" }}
+								/>
+								<button onClick={selectFile} className="import">
+									IMPORT CSV
+								</button>
 								<button className="save">SAVE</button>
 							</div>
 						</div>
 						<div className="divider"></div>
+						<PullRequests array={array}></PullRequests>
 					</div>
 				</div>
 			</div>
 		</>
 	);
 }
+
+function upload(e, setArray) {
+	const file = e.target.files[0];
+	const fileReader = new FileReader();
+	if (file) {
+		fileReader.onload = function (event) {
+			const csvOutput = event.target.result;
+			loadCsv(csvOutput, setArray);
+		};
+
+		fileReader.readAsText(file);
+	}
+}
+
+function loadCsv(string, setArray) {
+	setArray(Papa.parse(string, { header: true }).data);
+}
+
+function PullRequests(array) {
+	const pulls = [];
+	console.log(array);
+	if (array !== undefined && array.array != undefined) {
+		for (var i = 0; i < array.array.length; i++) {
+			pulls.push(<PullRequestLayout key={i} array={array.array} i={i}></PullRequestLayout>);
+		}
+	}
+	console.log(pulls);
+	return pulls;
+}
+
+function PullRequestLayout({array,i}) {
+	
+	return(<div className="pr">
+		<div className="pr-checkbox"></div>
+		<div className="pr-header" onClick={()=>toggleClass(i)}>
+			<p className="pr-id">#{array[i]["#"]}</p>
+			<p className="pr-title">{array[i].Title}</p>
+			<p className="pr-author"> - {array[i].User}</p>
+		</div>
+		<div id={"id-"+i} className={["pr-content","hidden"].join(' ')}>
+			<p className="pr-body"><ReactMarkdown>{array[i].Body}</ReactMarkdown></p>
+			<a className="pr-url" href={array[i].URL}>{array[i].URL}</a>
+			<p className="pr-repository">
+				<b>Repo: </b>
+				{array[i].Repository}
+			</p>
+			<p className="pr-created">
+				<b>Openned at: </b>
+				{array[i].Created}
+			</p>
+		</div>
+	</div>)
+}
+
+function toggleClass(i) {
+    const oldClassName = document.getElementById('id-'+i).className;
+    const newClassName = oldClassName === 'pr-content hidden' ? 'pr-content visible' : 'pr-content hidden'
+    document.getElementById('id-'+i).className = newClassName
+  }
+
+function showContent() {}
 
 const styles = {
 	button: {
