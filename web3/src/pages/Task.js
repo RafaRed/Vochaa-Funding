e7ec1be0d85e88260c0b86fd1f6e48756912cde6
RafaRed <rafaelszcardoso@gmail.com>
2022-03-30 23:54:20 -0300

    New card layout for PRs on repository page

--- web3/src/pages/Task.js
+++ web3/src/pages/Task.js
@@ -6,15 +6,18 @@ import { useParams } from "react-router-dom";
 import { getAllPullrequestsVotes, getTask } from "../model/Calls/Database";
 import moment from "moment";
 import ReactMarkdown from "react-markdown";
-import seedrandom from 'seedrandom'
+import seedrandom from "seedrandom";
 import "../css/Task.css";
 
-var formatter = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' });
+var formatter = new Intl.NumberFormat("de-DE", {
+	style: "currency",
+	currency: "EUR",
+});
 
 function Task() {
 	const [username, setUsername] = useState("");
 	const params = useParams();
-	
+
 	const [search, setSearch] = useState();
 	const [votesList, setVotesList] = useState({});
 	const [tasks, setTasks] = useState({
@@ -24,16 +27,10 @@ function Task() {
 		"contest-endDate": 0,
 	});
 	useEffect(() => {
-		loadVotes(params.contest,params.task,setVotesList)
+		loadVotes(params.contest, params.task, setVotesList);
 		loadTask(params, setTasks);
-		
 	}, []);
 	var seed = 741852963;
-
-
-
-	
-	
 
 	return (
 		<>
@@ -54,7 +51,6 @@ function Task() {
 									</p>
 									<a className="task-url" href={tasks["url"]}>
 										<p>{tasks["url"]}</p>
-										
 									</a>
 								</div>
 							</div>
@@ -65,7 +61,9 @@ function Task() {
 							<h2 className="contest-name">{tasks["contest-name"]}</h2>
 							<p className="contest-pool">Funding Pool</p>
 							<h2 className="contest-pool-value">
-								{formatter.format(tasks["contest-funding"]).replace("€",tasks["contest-currency"])}
+								{formatter
+									.format(tasks["contest-funding"])
+									.replace("€", tasks["contest-currency"])}
 							</h2>
 						</div>
 					</div>
@@ -80,7 +78,12 @@ function Task() {
 						<img src="/images/search.png" />
 					</div>
 					<div className="board">
-						<FetchPullRequests params={params} tasks={tasks} search={search} seed={seed} votesList={votesList}></FetchPullRequests>
+						<FetchPullRequests
+							params={params}
+							tasks={tasks}
+							search={search}
+							seed={seed}
+							votesList={votesList}></FetchPullRequests>
 					</div>
 				</div>
 			</div>
@@ -92,55 +95,62 @@ function loadTask(params, setTasks) {
 	getTask(params.contest, params.task).then((data) => setTasks(data));
 }
 
-function FetchPullRequests({ params, tasks, search, seed, votesList}) {
+function FetchPullRequests({ params, tasks, search, seed, votesList }) {
 	var taskList = [];
-	var rng=seedrandom(seed);
-	
+	var rng = seedrandom(seed);
 
 	if (tasks !== undefined && tasks.pullrequests !== undefined) {
 		for (var i = 0; i < tasks.pullrequests.length; i++) {
-			if(isOnSearch(tasks.pullrequests[i], search)){
+			if (isOnSearch(tasks.pullrequests[i], search)) {
 				var votes;
-				var totalVotes = votesList['totalVotes']
-				var funding = votesList['funding']
-				if(votesList !== undefined && votesList.votes !== undefined)
-				{
-					votes = votesList.votes[tasks.pullrequests[i]['pr']]
+				var totalVotes = votesList["totalVotes"];
+				var funding = votesList["funding"];
+				if (votesList !== undefined && votesList.votes !== undefined) {
+					votes = votesList.votes[tasks.pullrequests[i]["pr"]];
 				}
-				if(votes === undefined){
-					votes = 0
+				if (votes === undefined) {
+					votes = 0;
 				}
 				//console.log("data:"+votes,totalVotes,funding)
 				var percentage = 0;
-				var valueMatch = formatter.format(0).replace("€",tasks["contest-currency"]);
-				if(votes !== 0 ){
+				var valueMatch = formatter
+					.format(0)
+					.replace("€", tasks["contest-currency"]);
+				if (votes !== 0) {
 					percentage = Math.round((votes / totalVotes) * 100);
-					valueMatch = formatter.format((funding / totalVotes) * votes).replace("€",tasks["contest-currency"])
+					valueMatch = formatter
+						.format((funding / totalVotes) * votes)
+						.replace("€", tasks["contest-currency"]);
 				}
-				
-				
+
 				//console.log("result:"+votes,percentage,valueMatch,tasks.pullrequests[i]['pr'])
-				taskList.push(PullRequest(params, tasks.pullrequests[i], votes, percentage, valueMatch));
+				taskList.push(
+					PullRequest(params, tasks.pullrequests[i], votes, percentage, valueMatch)
+				);
 			}
-			
 		}
 	}
 	taskList = taskList.sort(() => rng() - 0.5);
 	return taskList;
 }
 
-
-function isOnSearch(task, search){
-	if (search === undefined  || search===""|| searchCointains(task.title,search)|| searchCointains(task.pr,search) || searchCointains("#"+task.pr,search) || searchCointains(task.user,search)) {
-		return true
-	}
-	else{
+function isOnSearch(task, search) {
+	if (
+		search === undefined ||
+		search === "" ||
+		searchCointains(task.title, search) ||
+		searchCointains(task.pr, search) ||
+		searchCointains("#" + task.pr, search) ||
+		searchCointains(task.user, search)
+	) {
+		return true;
+	} else {
 		return false;
 	}
 }
 
-function searchCointains(string,search){
-	return string.toLowerCase().startsWith(search.toLowerCase())
+function searchCointains(string, search) {
+	return string.toLowerCase().startsWith(search.toLowerCase());
 }
 function PullRequest(params, task, votes, percentage, valueMatch) {
 	return (
@@ -158,23 +168,42 @@ function PullRequest(params, task, votes, percentage, valueMatch) {
 
 			<div className="divider"></div>
 
-			<p>Votes</p>
-			<p className="pr-contributors">{votes}</p>
-			<p>Reward</p>
-			<p>{valueMatch} {percentage}%</p>
+			<div className="vote-status" key={0}>
+			<div className="vote-status-match">
+					<p>
+						<b>Reward</b>
+					</p>
+					<p>{valueMatch}</p>
+				</div>
+				
+				<div className="vote-status-header">
+					<p>{votes} votes </p>
+					<p>{Math.round(percentage)}%</p>
+				</div>
+
+				<div className="progress-bar">
+					<div
+						className="progress-bar-status"
+						style={{ width: percentage + "%" }}></div>
+				</div>
+
+				
+			</div>
 			<a
 				className="view-button-wrapper"
-				href={"/contest/" + params.contest +"/"+params.task + "/"+task.pr}>
+				href={"/contest/" + params.contest + "/" + params.task + "/" + task.pr}>
 				<p className="view-button">View More</p>
 			</a>
+
+
 		</div>
 	);
 }
 
-
-function loadVotes(contestid,repositoryid,setVotesList){
-	getAllPullrequestsVotes(contestid,repositoryid)
-	.then(data => setVotesList(data))
+function loadVotes(contestid, repositoryid, setVotesList) {
+	getAllPullrequestsVotes(contestid, repositoryid).then((data) =>
+		setVotesList(data)
+	);
 }
 
 const styles = {
