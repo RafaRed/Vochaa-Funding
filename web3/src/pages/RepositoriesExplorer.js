318718b2b0df05cc6ef51457a5562cf0dce6e2c4
RafaRed <rafaelszcardoso@gmail.com>
2022-04-20 12:13:27 -0300

    Fixed code wrapping, added code background color, added repositories number to contest page

--- web3/src/pages/RepositoriesExplorer.js
+++ web3/src/pages/RepositoriesExplorer.js
@@ -3,13 +3,15 @@ import { useParams, useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import injectSheet from "react-jss";
 import "../css/RepositoriesExplorer.css";
-import { getContest, loadTasks } from "../model/Calls/Database";
+import {
+	getContest,
+	getPullRequests,
+	loadTasks,
+} from "../model/Calls/Database";
 import ReactMarkdown from "react-markdown";
 import { backButton, fetchContestCredits } from "../utils/utils";
 import { auth, provider } from "../model/firebaseConnect";
-import {
-	onAuthStateChanged,
-} from "firebase/auth";
+import { onAuthStateChanged } from "firebase/auth";
 
 function RepositoriesExplorer(props) {
 	const [username, setUsername] = useState("");
@@ -18,23 +20,27 @@ function RepositoriesExplorer(props) {
 	const [contest, setContest] = useState({});
 	const [description, setDescription] = useState();
 	const [readMore, setReadMore] = useState(false);
-	const [userCredits, setUserCredits] = useState()
+	const [userCredits, setUserCredits] = useState();
 	const linkName = readMore ? "Read Less << " : "Read More >> ";
 	useEffect(() => {
 		FetchContest(params.contest, setContest, setDescription);
-		
 	}, []);
 
 	useEffect(() => {
 		onAuthStateChanged(auth, (currentUser) => {
-			fetchContestCredits(params.contest,setUserCredits)
-			console.log(userCredits)
+			fetchContestCredits(params.contest, setUserCredits);
+			console.log(userCredits);
 		});
 	}, []);
 
 	return (
 		<>
-			<Navbar menu="explore" username={username} setUsername={setUsername} userCredits={userCredits} />
+			<Navbar
+				menu="explore"
+				username={username}
+				setUsername={setUsername}
+				userCredits={userCredits}
+			/>
 			<div className="repositories">
 				<div className="wrapper">
 					<div className="header">
@@ -88,27 +94,54 @@ function formatDescription(description, setDescription) {
 	newDescription = newDescription.replace("  ", "");
 	setDescription(newDescription);
 }
+
+function getRepositoryFromUrl(url) {
+	var repositoryPath = url.split("/");
+	repositoryPath =
+		repositoryPath[repositoryPath.length - 2] +
+		"/" +
+		repositoryPath[repositoryPath.length - 1];
+		return repositoryPath;
+}
+
 function LoadTasks({ params, taskList, setTaskList }) {
 	useEffect(() => {
 		var newTaskList = [];
-		loadTasks(params.contest).then((tasks) => {
-			for (const [key, value] of Object.entries(tasks)) {
-				newTaskList.push(Task(tasks[key], key, params.contest));
+		var repositories = {}
+		getPullRequests(params.contest).then((pullrequests) => {
+			
+			for (const [key, value] of Object.entries(pullrequests)) {
+				if(pullrequests[key].repository in repositories)
+				{
+					repositories[pullrequests[key].repository] ++;
+				}
+				else{
+					repositories[pullrequests[key].repository] = 1;
+				}
 			}
-			setTaskList(newTaskList);
+
+			loadTasks(params.contest).then((tasks) => {
+				for (const [key, value] of Object.entries(tasks)) {
+					console.log(tasks[key]);
+					var repositoryPath = getRepositoryFromUrl(tasks[key].url);
+					newTaskList.push(Task(tasks[key], key, params.contest, repositories[repositoryPath]));
+				}
+				setTaskList(newTaskList);
+			});
 		});
+		
 	}, []);
 
 	return taskList;
 }
 
-function Task(task, key, contestid) {
+function Task(task, key, contestid, repositories) {
 	return (
 		<a
 			className="contest-button"
 			key={key}
 			href={"/contest/" + contestid + "/" + key + "/"}>
-			<div className="repo-task">{task["name"]}</div>
+			<div className="repo-task">{task["name"]} ({repositories})</div>
 		</a>
 	);
 }
